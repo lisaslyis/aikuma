16421f8078a8c99db1b6e87e804c7b209867c60f
Oliver Adams <oliver.adams@gmail.com>
2013-12-12 12:44:55 +1100

    added the help activity, but yet to add the content and formatting.

---
+++ Aikuma/src/org/lp20/aikuma/ui/HelpActivity.java
@@ -0,0 +1,11 @@
+package org.lp20.aikuma.ui;
+
+import android.os.Bundle;
+
+public class HelpActivity extends AikumaActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+	}
+}
